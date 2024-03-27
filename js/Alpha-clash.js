bc463c75025625ad518a87e1056acf74f445c2c4
Mobarak Hossain <mobarakhossain121838@gmail.com>
2024-03-27 11:51:51 +0600

    score and life updated

--- js/Alpha-clash.js
+++ js/Alpha-clash.js
@@ -7,10 +7,57 @@
 // const playGroundSection = document.getElementById('play-ground');
 // playGroundSection.classList.remove('hidden');
 // }
+function handleKeyboardButtonPress(event){
+const playerPressed = event.key;
+console.log('player pressed', playerPressed);
+
+// get the expected to press
+const currentAlphabetElement = document.getElementById('current-alphabet')
+const currentAlphabet = currentAlphabetElement.innerText;
+const expectedAlphabet = currentAlphabet.toLowerCase();
+console.log(playerPressed, expectedAlphabet);
+
+// check match or not 
+if (playerPressed === expectedAlphabet) {
+    console.log('you got a point');
+    // update score:
+    // 1.get the current score
+    const currentScoreElement = document.getElementById('current-score');
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+    // 2.increase the score by 1
+    const newScore = currentScore + 1;
+    // 3. show the update score
+    currentScoreElement.innerText = newScore;
+    removeBackgroundColor(expectedAlphabet);
+    continueGame();
+} else {
+    console.log('you missed.you lost a life');
+    const currentLifeElement = document.getElementById('current-life');
+    const currentLifeText = currentLifeElement.innerText;
+    const currentLife = parseInt(currentLifeText);
+
+    const newLife = currentLife - 1;
+    currentLifeElement.innerText = newLife;
+    
+}
+
+}
+
+document.addEventListener('keyup',handleKeyboardButtonPress)
+
 
 function continueGame(){
  const alphabet = getARandomAlphabet();
- console.log(alphabet);
+//  console.log(alphabet);
+
+//  set randomly generated alphabet to the screen (show it)
+
+const currentAlphabet = document.getElementById('current-alphabet');
+currentAlphabet.innerText = alphabet;
+
+setBackgroundColor(alphabet);
 
 }
 
--- js/utility.js
+++ js/utility.js
@@ -19,3 +19,13 @@ function getARandomAlphabet(){
     return alphabet
 
 }
+
+function setBackgroundColor(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.add('bg-orange-400');
+}
+
+function removeBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
